c28d4a22b2c63cafea5740abc732a6841e4a5948
JnuMxin <JnuMxin@users.noreply.github.com>
2018-03-22 00:17:37 +0800

    修改为用Xpath查找控件，增加js部分，利用完善代码功能
    selenium要移动到控件处才可以点击空间，因此加入了先运行js的代码
    利用Xpath来定位控件更好
    链接抢的是林俊杰的票

--- damai.py
+++ damai.py
@@ -13,9 +13,9 @@ from selenium.common.exceptions import TimeoutException
 import time
 
 # 设置抢票链接和开票时间
-URL = "https://piao.damai.cn/138382.html?spm=a2o6e.search.0.0.2b5328df7CB40F"
-HOUR = 11
-MIN  = 18
+URL = "https://piao.damai.cn/145760.html?spm=a2o6e.search.0.0.2bd01e33HuTSpM"
+HOUR = 20
+MIN  = 0
 
 driver = webdriver.Chrome()
 # 设置等待时间
@@ -23,14 +23,18 @@ wait = WebDriverWait(driver, 5)
 # 以周杰伦的为例
 driver.get(URL)
 
-# test
-# driver.get("https://piao.damai.cn/141699.html?spm=a2o6e.search.0.0.10f94d15pF81cd")
+# 用户名
+USERNAME = "13112390306"
 
+driver = webdriver.Chrome()
+# 设置等待时间
+wait = WebDriverWait(driver, 5)
+driver.get(URL)
 
 def choose(seletor):
     try:
         # 控件可点击时才选定
-        choice = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, seletor)))
+        choice = wait.until(EC.element_to_be_clickable((By.XPATH, seletor)))
         return choice
     except TimeoutException as e:
         print("Time out!")
@@ -39,51 +43,66 @@ def choose(seletor):
         print("Not found!")
         return None
 
-# 点击登录
-login = choose("#userLoginInfo > span > a:nth-child(1)")
-login.click()
-username = choose("#login_email")
-username.send_keys("13112390306")
-"""
-由于密码框控件被设置为不可见
-先自行输入密码并记住密码
-方便刷新
-（也可用cookie实现）
-"""
+def login():
+    # 点击登录
+    login = choose('//*[@id="userLoginInfo"]/span/a[1]')
+    login.click()
+    username = choose('//*[@id="login_email"]')
+    username.send_keys(USERNAME)
+    """
+    由于密码框控件被设置为不可见
+    先自行输入密码并记住密码
+    方便刷新
+    （也可用cookie实现）
+    """
+    password = choose('//*[@id="login_pwd_txt"]')
+    try:
+        password.click()
+        password.send_keys("haobang058")
+    except Exception:
+        print(password)
+        print("Password Can't click")
 
-# 10秒等待用户输入密码后再开始刷
-time.sleep(10)
+def buy():
+    # 点击价格
+    try:
+        price = None
+        plus = None
+        buybtn = None
+        submit = None
+        driver.get(URL)
+        # 选择价格
+        while None == price:
+            # 这里选的是580票面的，如果选其他票面，修改最后的li[*]即可
+            price = choose('//*[@id="priceList"]/div/ul/li[2]')
+        price.click()
+        # 数量加1
+        while None == plus:
+            plus = choose('//*[@id="cartList"]/div[1]/ul/li/span[3]/a[2]')
+        plus.click()
+        # 立即抢购
+        while None == buybtn:
+            buybtn = choose('//*[@id="btnBuyNow"]')
+        # 运行JS滑动到控件处再点击
+        driver.execute_script("arguments[0].scrollIntoView();", buybtn) 
+        buybtn.click()
+        # 提交订单
+        while None == submit:
+            submit = choose('//*[@id="orderConfirmSubmit"]')
+        driver.execute_script("arguments[0].scrollIntoView();", submit) 
+        submit.click()
+    except Exception:
+        print("抢票失败，尝试重新抢票")
+        buy()
 
-while 1:
-    if HOUR == time.localtime().tm_hour:
-        while 1:
-            if MIN == time.localtime().tm_min:
-                print("开始抢票")
-                driver.get(URL)
-                price = None
-                plus = None
-                buybtn = None
-                submit = None
-                # 点击价格
-                while None == price:
-                    # 这里选的是580票面的，如果选其他票面，修改对应的选择器内容即可
-                    price = choose("li.itm-oos:nth-child(2)")
-                print(price)
-                price.click()
-                # 数量加1
-                while None == plus:
-                    plus = choose("a.btn:nth-child(3)")
-                plus.click()
-                # 立即抢购
-                while None == buybtn:
-                    buybtn = choose("#btnBuyNow")
-                buybtn.click()
-                # 提交订单
-                while None == submit:
-                    submit = choose("#orderConfirmSubmit")
-                submit.click()
-                break
-   
-time.sleep(10)
-# driver.quit()
-price("抢票成功")
+
+if __name__ == '__main__':
+    login()
+    # 20秒等待用户输入密码后再开始刷
+    time.sleep(20)
+
+    while 1:
+        if MIN == time.localtime().tm_min:
+            print("开始抢票")
+            buy()
+            print("抢票成功")
